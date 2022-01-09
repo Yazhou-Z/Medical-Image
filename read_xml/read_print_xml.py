6beaf8cba9d8615288130213614290d743cce80a
Yazhou-Z <76484768+Yazhou-Z@users.noreply.github.com>
2022-01-10 00:59:15 +0800

    get all patients' ROIs

--- read_xml/read_print_xml.py
+++ read_xml/read_print_xml.py
@@ -1,33 +1,54 @@
 from xml.dom import minidom
 from pandas import DataFrame
-dom = minidom.parse("catROIs_00014.xml") #打开xml
+import os
+from openpyxl import Workbook
+ 
+def make_xlsx(path):
+    wb = Workbook()
+    wb.save(path)
 
-def getInfo(node):
+
+def getInfo(node, dom):
     names = dom.getElementsByTagName(node)
     data = names[0].firstChild.data
     #l = len(data)
     #print(data, l)
     data = data[1:-2].split(';')    # delete "[" and "]"
-    l = len(data)
-    print(l)
+    # l = len(data)
+    # print(l)
     return data
     # for ii in range(l): print(data[ii])
 
-excel ={}
 
-# 打印各脑区id与四个特征
-informations = ['ids', 'depth', 'fractaldimension', 'gyrification', 'thickness']
-for node in informations:
-    print(node, ': ')
-    excel[node] = getInfo(node)
+def set_up_excel(filename):
+    excel ={}
+    dom = minidom.parse("label/" + filename + ".xml") #打开xml
 
-# 打印各脑区名称
-names = dom.getElementsByTagName('item')
-name = []
-for i in range(len(names)-1): name.append(names[i].firstChild.data)
-print(name, len(names))
+    # 打印各脑区id与四个特征
+    informations = ['ids', 
+    # 'depth', 'fractaldimension', 'gyrification', 
+    'thickness']
+    for node in informations:
+        print(node, ': ')
+        excel[node] = getInfo(node, dom)
 
-excel['name'] = name
-print(excel)
-df = DataFrame(excel)
-df.to_excel('test.xlsx')
+    # 打印各脑区名称
+    names = dom.getElementsByTagName('item')
+    name = []
+    for i in range(152): name.append(names[i].firstChild.data)
+    # print(name, len(names))
+
+    excel['name'] = name
+    print(len(excel['name']))
+    df = DataFrame(excel)
+    xlsx_path = filename + ".xlsx"
+    make_xlsx(xlsx_path)
+    df.to_excel(xlsx_path)
+
+    return 0
+
+for i in range(15, 27):
+    s = "%05d" % i
+    data = "catROIs_" + s
+    set_up_excel(data)
+
